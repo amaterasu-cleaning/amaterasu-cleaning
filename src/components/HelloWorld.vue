4a8079911d3fe795ac3e273a37bbc38f5c23af28
Shohei Imamura <s3imamura@gmail.com>
2021-03-29 00:35:14 +0900

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12"> 掃除報告ー </v-col>
+      <v-col cols="12">
+        <v-treeview
+          selectable
+          selected-color="red"
+          :items="items"
+          open-all
+        ></v-treeview>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12"> <v-btn dark>掃除したよー</v-btn> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        name: "1階",
+        children: [
+          { id: 2, name: "デカリビング" },
+          { id: 3, name: "デカキッチン" },
+          { id: 4, name: "チビリビング" },
+          { id: 5, name: "チビキッチン" },
+          { id: 6, name: "廊下" },
+          { id: 7, name: "トイレ（右）" },
+          { id: 8, name: "洗面所" },
+        ],
+      },
+      {
+        id: 9,
+        name: "2階",
+        children: [
+          { id: 10, name: "階段" },
+          { id: 11, name: "廊下" },
+          { id: 12, name: "男子トイレ" },
+          { id: 13, name: "女子トイレ" },
+          { id: 14, name: "シャワールーム（奥）" },
+          { id: 15, name: "シャワールーム（手前）" },
+          { id: 16, name: "鏡の間" },
+        ],
+      },
+      {
+        id: 17,
+        name: "その他",
+        children: [
+          { id: 18, name: "玄関" },
+          { id: 19, name: "駐輪場" },
+          { id: 20, name: "デッキ" },
+          { id: 21, name: "物干し場" },
+        ],
+      },
+    ],
+  }),
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
